f5670253e31160183880515add551fe9e7593e0a
Brian Teets <bwt17@msn.com>
2022-02-11 15:35:04 -0700

    3:34pm Feb 11, 2022

---
+++ classwork/section6-8-1-4-challenge-array-loop.js
@@ -0,0 +1,13 @@
+/*
+Multiply each element in sourceArray with the corresponding value in offsetArray. Store each value in a new array named finalArray.
+*/
+let sourceArray = [ 40, 50, 60, 70 ]; // Tests will use different arrays
+let offsetArray = [ 2, 5, 8, 4 ]; // Tests will use different arrays
+let finalArray = [];
+
+/* Your solution goes here */
+for (let i = 0; i < finalArray.length; i++) {
+      finalArray = sourceArray[i] * offsetArray[i];
+   }
+
+console.log(finalArray);
